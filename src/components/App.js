26015e58abd9c3d9dd9ba621bdbd89cb26755d46
madhur <madhurpanghal0001@gmail.com>
2025-12-03 13:55:27 +0530

    2

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,14 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <div onMouseEnter={() => setCurrent(1)} onMouseLeave={() => setCurrent(0)}>
-        { current===1 && <Tooltip current={current} />}
-        <h2 >Hover over me</h2>
+      <div>
+        
+        <h2 className="tooltip" onMouseEnter={() => setCurrent(1)} onMouseLeave={() => setCurrent(0)}>{ current===1 && <Tooltip current={current} />} Hover over me </h2>
       </div>
       <hr />
-      <div onMouseEnter={() => setCurrent(2)} onMouseLeave={() => setCurrent(0)}>
-        { current===2 && <Tooltip current={current} />}
-        <p >Hover over me to see another tooltip</p>
+      <div >
+        
+        <p className="tooltip" onMouseEnter={() => setCurrent(2)} onMouseLeave={() => setCurrent(0)}>{ current===2 && <Tooltip current={current} />} Hover over me to see another tooltip </p>
       </div>
     </div>
   )
